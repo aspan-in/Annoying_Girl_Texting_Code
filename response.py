699c14aba028d071fc744469d2a92d562226927b
Aseem Pandya <35763344+aspan-in@users.noreply.github.com>
2021-10-09 15:24:50 +0530

    Update response.py

--- response.py
+++ response.py
@@ -1,6 +1,6 @@
 import random
 while True:
     You=input("You:")
-    rand=["nice","yeah","fine","i don't know what to say","hmm","you know what","let's talk tomorrow","I need some space","oh","huh?","I am fine","nothing","kuch nahi","idk","yeah maybe","yeah right"]
-    ch=random.randint(0,8)
+    rand=["nice","yeah","fine","i don't know what to say","hmm","you know what","let's talk tomorrow","I need some space","oh","huh?","I am fine","nothing","kuch nahi","idk","yeah maybe","yeah right","yup","no"]
+    ch=random.randint(0,10)
     print("She:",rand[ch])
