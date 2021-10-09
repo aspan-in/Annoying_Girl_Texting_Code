0ee8d2d556cbd5a40998b893c6b050d225032c75
Aseem Pandya <35763344+aspan-in@users.noreply.github.com>
2021-10-09 15:16:31 +0530

    Update response.py

--- response.py
+++ response.py
@@ -1,6 +1,6 @@
 import random
 while True:
-    Aseem=input("Aseem:")
+    You=input("You:")
     rand=["nice","yeah","fine","i don't know what to say","hmm","you know what","let's talk tomorrow","I need some space","oh","huh?","I am fine","nothing","kuch nahi","idk","yeah maybe","yeah right"]
     ch=random.randint(0,8)
-    print("Shivani:",rand[ch])
+    print("She:",rand[ch])
