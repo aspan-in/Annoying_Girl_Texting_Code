e6df4dc868afb4bab3eae62a8c155211e91ef354
Aseem Pandya <35763344+aspan-in@users.noreply.github.com>
2021-11-08 21:25:09 +0530

    Update response.py

--- response.py
+++ response.py
@@ -1,5 +1,6 @@
 import random
 print("Texting Platform... Input Your Texts and get auto replies....")
+print('')
 while True:
     You=input("You:")
     rand=["nice","yeah","fine","i don't know what to say","hmm","you know what","let's talk tomorrow","I need some space","oh","huh?","I am fine","nothing","kuch nahi","idk","yeah maybe","yeah right","yup","no"]
